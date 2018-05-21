17a042caafc8a337947bd3ed34b93ee945904038
mfrashad <m.fathyrashad@gmail.com>
2018-05-22 00:48:15 +0800

    Add spec helper

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -11,5 +11,51 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe PostsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#create_new_post_partial_path' do
+    it 'returns a signed_in_partial_path' do
+      helper.stub(:user_signed_in?).and_return(true)
+      expect(helper.create_new_post_partial_path).to eq (
+        'posts/branch/create_new_post/signed_in'
+      )
+    end
+
+    it 'returns a not_signed_in_partial_path' do
+      helper.stub(:user_signed_in?).and_return(false)
+      expect(helper.create_new_post_partial_path).to eq (
+        'posts/branch/create_new_post/not_signed_in'
+      )
+    end
+  end
+
+  context '#all_categories_button_partial_path' do
+    it 'returns all_selected partial path' do
+      controller.params[:category] = ''
+      expect(helper.all_categories_button_partial_path).to eq (
+        'posts/branch/categories/all_selected'
+      )
+    end
+
+    it 'returns all_not_selected partial path' do
+      controller.params[:category] = 'category'
+      expect(helper.all_categories_button_partial_path).to eq (
+        'posts/branch/categories/all_not_selected'
+      )
+    end
+  end
+
+  context '#no_post_partial_path' do
+    it 'returns no_post partial path' do
+      assign(:posts, [])
+      expect(helper.no_posts_partial_path).to eq (
+        'posts/branch/no_posts'
+      )
+    end
+
+    it 'returns empty_partial' do
+      assign(:posts, [1])
+      expect(helper.no_posts_partial_path).to eq (
+        'shared/empty_partial'
+      )
+    end
+  end
 end
