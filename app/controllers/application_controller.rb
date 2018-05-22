13c0d25eaa5e6e6a1c8d299b9d8002cedf026892
mfrashad <m.fathyrashad@gmail.com>
2018-05-22 14:29:37 +0800

    create ui for new post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  def redirect_if_not_signed_in
+    redirect_to root_path if !user_signed_in?
+  end
+  
+  def redirect_if_signed_in
+    redirect_to root_path if user_signed_in?
+  end
   protect_from_forgery with: :exception
 end
