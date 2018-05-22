dbc4e6ff1ef8eaa2fa22c6ab531382e1ffa5f8d9
mfrashad <m.fathyrashad@gmail.com>
2018-05-22 13:27:45 +0800

    add All post in one branch

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -14,8 +14,8 @@ module PostsHelper
       'posts/branch/categories/all_not_selected'
     end
   end
-  def no_posts_partial_path
-    @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
+  def no_posts_partial_path(posts)
+    posts.empty? ? 'posts/shared/no_posts' : 'shared/empty_partial'
   end
 
   def post_format_partial_path
@@ -31,10 +31,10 @@ module PostsHelper
   end
 
   def update_pagination_partial_path
-  if @posts.next_page
-    'posts/posts_pagination_page/update_pagination'
-  else
-    'posts/posts_pagination_page/remove_pagination'
+    if @posts.next_page
+      'posts/posts_pagination_page/update_pagination'
+    else
+      'posts/posts_pagination_page/remove_pagination'
+    end
   end
-end
 end
